f60cecca23dc7c9dcf52f5577fe8c0bc54ab2aba
Qmagician <2544762445@qq.com>
2019-04-01 23:21:04 +0800

    项目初始化

---
+++ Vue/Function-Instance/src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <h1>Vue 项目功能实例</h1>
+
+    <el-table :data="tableData" border style="width: 80%; margin-left: 10%;" >
+    <el-table-column prop="function" label="功能" min-width="20%" align="center">
+    </el-table-column>
+    <el-table-column prop="description" label="描述" min-width="65%" align="center">
+    </el-table-column>
+    <el-table-column prop="link" label="跳转" min-width="15%" align="center">
+      <el-button type='primary' @click="gotoScreenShotUpload">截图上传</el-button>
+    </el-table-column>
+  </el-table>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      tableData: [{
+          function: '截图上传',
+          description: '截图上传，在上传图片前可先对图片进行剪切，再上传。',
+        }, ]
+    };
+  },
+
+  methods: {
+    gotoScreenShotUpload()
+    {
+      this.$router.push({path:'/ScreenShotUpload'});
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+
+</style>
